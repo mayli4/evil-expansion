4aafc4d069ea062f0c939140e13e8c1d44e2731a
mayli4 <sydsleeper@gmail.com>
2025-07-24 00:28:28 -0400

    feat: new effigy visuals

---
+++ src/EvilExpansionMod/Assets/Effects/Pixel/EffigyDecay.fx
@@ -0,0 +1,64 @@
+﻿sampler uImage0 : register(s0);
+
+texture noisetex;
+sampler2D fireNoise = sampler_state {
+    texture = <noisetex>;
+};
+
+float prog;
+
+float3 edgeColor;
+float3 ashColor;
+
+float noiseTexelAspect;
+float frameTexelAspect;
+
+float2 texSize;
+
+float4 sampleColor;
+
+float4 PixelShaderFunction(float2 coords : TEXCOORD0) : COLOR0 {
+    float4 material = tex2D(uImage0, coords);
+
+    if (material.a < 0.005) {
+        return float4(0.0, 0.0, 0.0, 0.0);
+    }
+    
+    float2 noiseSampleCoords = coords;
+
+    float effectiveAspect = frameTexelAspect / noiseTexelAspect;
+
+    if (effectiveAspect < 1.0) {
+        noiseSampleCoords.x /= effectiveAspect;
+        noiseSampleCoords.x -= (1.0 / effectiveAspect - 1.0) * 0.5;
+    } else if (effectiveAspect > 1.0) {
+
+        noiseSampleCoords.y *= effectiveAspect;
+        noiseSampleCoords.y -= (effectiveAspect - 1.0) * 0.5;
+    }
+    
+	float2 pixelSize = 1.5 / texSize;
+    noiseSampleCoords = round(noiseSampleCoords / pixelSize) * pixelSize;
+    
+    float noiseValue = tex2D(fireNoise, noiseSampleCoords).r;
+    
+    float erosionAmount = smoothstep(prog - 0.2, prog, noiseValue);
+    float borderStrength = smoothstep(0.0, 0.5, erosionAmount) - smoothstep(0.3, 1.0, erosionAmount);
+    
+    float3 final_rgb = material.rgb * (1.0 - erosionAmount);
+    
+    float3 fire = lerp(edgeColor, ashColor, smoothstep(0.7, 1., borderStrength)) * 3.0;
+
+    final_rgb += borderStrength * fire;
+    
+    float final_alpha = material.a * (1.0 - erosionAmount);
+    final_alpha = max(final_alpha, borderStrength);
+    
+    return float4(final_rgb, final_alpha) * sampleColor;
+}
+
+technique Technique1 {
+    pass VignettePass {
+        PixelShader = compile ps_2_0 PixelShaderFunction();
+    }
+};
--- src/EvilExpansionMod/Content/NPCs/Corruption/_Effigy/EffigyNPC.cs
+++ src/EvilExpansionMod/Content/NPCs/Corruption/_Effigy/EffigyNPC.cs
@@ -1,6 +1,8 @@
 ﻿using EvilExpansionMod.Content.Biomes;
+using EvilExpansionMod.Utilities;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using Terraria;
 using Terraria.GameContent;
 using Terraria.ID;
@@ -12,20 +14,29 @@ public sealed class EffigyNPC : ModNPC {
     byte _spawnedSprits = 0;
 
     public override string Texture => Assets.Assets.Textures.NPCs.Corruption.Effigy.KEY_EffigyNPC;
+
+    private bool _dead;
+    private int _deadTimer;
+    private int _animCounter;
+    
+    public int DEATH_TIME = 5 * 60;
+
+    private Color _glowColor = new Color(230, 254, 6);
     
     public override void SetStaticDefaults() {
         Main.npcFrameCount[Type] = 18;
     }
     
     public override void SetDefaults() {
-        NPC.width = 36;
+        NPC.width = 60;
         NPC.height = 90;
+        
         NPC.lifeMax = 640;
         NPC.value = 250f;
         NPC.noTileCollide = false;
         NPC.aiStyle = -1;
-        NPC.noGravity = true;
-        NPC.knockBackResist = 0.05f;
+        NPC.noGravity = false;
+        NPC.knockBackResist = 0f;
         NPC.damage = 0;
         NPC.friendly = false;
 
@@ -38,7 +49,18 @@ public sealed class EffigyNPC : ModNPC {
         NPC.lavaImmune = true;
     }
 
-    public override void AI() { }
+    public override void AI() {
+        if(_dead) {
+            _deadTimer++;
+            _animCounter++;
+            Lighting.AddLight(NPC.Center, _glowColor.ToVector3());
+            
+            if(_deadTimer >= DEATH_TIME) {
+                NPC.life = 0;
+                NPC.active = false;
+            }
+        }
+    }
 
     void SpawnSpirit(Entity attacker) {
         var position = NPC.position + Vector2.UnitX * NPC.width / 2f;
@@ -48,25 +70,101 @@ public sealed class EffigyNPC : ModNPC {
     }
 
     public override void OnHitByProjectile(Projectile projectile, NPC.HitInfo hit, int damageDone) {
-        SpawnSpirit(projectile);
+        //SpawnSpirit(projectile);
     }
 
     public override void OnHitByItem(Player player, Item item, NPC.HitInfo hit, int damageDone) {
-        SpawnSpirit(player);
+        //SpawnSpirit(player);
     }
 
     public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor) {
         var texture = TextureAssets.Npc[Type].Value;
-        Main.EntitySpriteDraw(
+        var glowTex = Assets.Assets.Textures.NPCs.Corruption.Effigy.EffigyNPC_Glow.Value;
+        
+        var offset = new Vector2(0, -77); //cause frame very big! yes
+
+        var shader = Assets.Assets.Effects.Compiled.Pixel.EffigyDecay.Value;
+
+        float progValue = 1.5f;
+        
+        if (NPC.frameCounter == 17) {
+            if(_animCounter >= 1.5f * 60) {
+                float deathProgress = MathHelper.Clamp((float)_deadTimer / DEATH_TIME, 0f, 1f);
+                progValue = MathHelper.Lerp(1.1f, 0.0f, deathProgress);
+            }
+        } else {
+            progValue = 1.5f;
+        }
+        
+        shader.Parameters["prog"].SetValue(progValue);
+        shader.Parameters["edgeColor"].SetValue(Color.Black.ToVector3());
+        shader.Parameters["ashColor"].SetValue(_glowColor.ToVector3());
+        shader.Parameters["noisetex"].SetValue(Assets.Assets.Textures.Sample.DissolveNoise.Value);
+        shader.Parameters["sampleColor"].SetValue(drawColor.ToVector4());
+        
+        var noiseTexture = Assets.Assets.Textures.Sample.DissolveNoise.Value;
+        float noiseAspect = (float)noiseTexture.Width / noiseTexture.Height;
+        float frameAspect = (float)NPC.frame.Width / NPC.frame.Height;
+
+        shader.Parameters["noiseTexelAspect"].SetValue(noiseAspect + 200);
+        shader.Parameters["frameTexelAspect"].SetValue(frameAspect + 2000);
+        shader.Parameters["texSize"].SetValue(new Vector2(NPC.frame.Width, NPC.frame.Height));
+        
+        var shaderSnapshot = spriteBatch.CaptureEndBegin(new() { CustomEffect = shader });
+        
+        spriteBatch.Draw(
             texture,
-            NPC.Center - screenPos,
+            NPC.Center + offset - screenPos,
             NPC.frame,
             drawColor,
-            0,
+            0f,
             NPC.frame.Size() / 2f,
             NPC.scale,
-            NPC.direction == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally
+            SpriteEffects.None,
+            0
         );
+        spriteBatch.EndBegin(shaderSnapshot);
+        
+        var snapshot = spriteBatch.CaptureEndBegin(new() { BlendState = BlendState.Additive, SamplerState = SamplerState.PointClamp});
+        spriteBatch.Draw(
+            glowTex,
+            NPC.Center + offset - screenPos,
+            new Rectangle(NPC.frame.X, NPC.frame.Y, NPC.frame.Width + 24, NPC.frame.Height),
+            _glowColor,
+            0f,
+            new Rectangle(NPC.frame.X - 12, NPC.frame.Y, NPC.frame.Width + 24, NPC.frame.Height).Size() / 2f,
+            1.0f,
+            SpriteEffects.None,
+            0
+        );
+        spriteBatch.EndBegin(snapshot);
+        
         return false;
+    }
+
+    public override bool CheckDead() {
+        if(_dead) return true;
+
+        _dead = true;
+        _deadTimer = 0;
+        
+        NPC.dontTakeDamage = true;
+        NPC.life = 1;
+
+        return false;
+    }
+
+    public override void FindFrame(int frameHeight) {
+        if(_dead) {
+            NPC.frameCounter += 0.20f;
+            if (NPC.frameCounter >= 17)
+                NPC.frameCounter = 17;
+        }
+        else {
+            NPC.frameCounter += 0.15f;
+            if (NPC.frameCounter >= 3)
+                NPC.frameCounter = 0;
+        }
+        NPC.frame.Y = (int)NPC.frameCounter * frameHeight;
     }
 }

93b2b2eb6a4d386e1167f6c35e4fd37fab0084d1
mayli4 <sydsleeper@gmail.com>
2025-08-07 21:45:35 -0400

    feat: armor now summons warden projectile

---
+++ src/EvilExpansionMod/Content/Items/Crimson/_MeatPrisonArmor/BloodWarden.cs
@@ -0,0 +1,7 @@
+using Terraria.ModLoader;
+
+namespace EvilExpansionMod.Content.Items.Crimson;
+
+public sealed class BloodWarden : ModProjectile {
+    public override string Texture => Assets.Assets.Textures.Items.Crimson.MeatPrisonArmor.KEY_BloodWarden;
+}
--- src/EvilExpansionMod/Content/Items/Crimson/_MeatPrisonArmor/MeatPrisonArmor.cs
+++ src/EvilExpansionMod/Content/Items/Crimson/_MeatPrisonArmor/MeatPrisonArmor.cs
@@ -1,3 +1,4 @@
+using Microsoft.Xna.Framework;
 using System.Collections.Generic;
 using Terraria;
 using Terraria.GameContent.Creative;
@@ -30,6 +31,7 @@ public class MeatPrisonHead : ModItem {
 
     public override void UpdateArmorSet(Player player) {
         player.setBonus = "Summons a blood warden to fight for you";
+        player.AddBuff(ModContent.BuffType<BloodWardenBuff>(), 2);
     }
 
     public override bool IsArmorSet(Item head, Item body, Item legs) {
@@ -96,5 +98,68 @@ public class MeatPrisonLegs : ModItem {
 }
 
 public sealed class MeatPrisonPlayer : ModPlayer {
+    public bool SetBonusActive => Player.armor[0].type == ModContent.ItemType<MeatPrisonHead>() 
+                                  && Player.armor[1].type == ModContent.ItemType<MeatPrisonBody>() 
+                                  && Player.armor[2].type == ModContent.ItemType<MeatPrisonLegs>();
+
+    public override void ResetEffects() {
+        if (!SetBonusActive)
+            Player.ClearBuff(ModContent.BuffType<BloodWardenBuff>());
+    }
     
+    public override void PostUpdateEquips() {
+        if (Player.whoAmI != Main.myPlayer) return;
+
+        if (Player.HasBuff<BloodWardenBuff>()) {
+            var foundBloodWarden = false;
+            for (int i = 0; i < Main.maxProjectiles; i++) {
+                Projectile proj = Main.projectile[i];
+                if (proj.active && proj.owner == Player.whoAmI && proj.type == ModContent.ProjectileType<BloodWarden>())
+                {
+                    foundBloodWarden = true;
+                    break;
+                }
+            }
+
+            if (!foundBloodWarden) {
+                Projectile.NewProjectile(
+                    Player.GetSource_Accessory(Player.armor[0]),
+                    Player.Center,
+                    Vector2.Zero,
+                    ModContent.ProjectileType<BloodWarden>(),
+                    (int)Player.GetDamage(DamageClass.Summon).Multiplicative,
+                    0f,
+                    Player.whoAmI
+                );
+            }
+        }
+    }
+}
+
+public class BloodWardenBuff : ModBuff {
+    public override string Texture => Assets.Assets.Textures.Items.Crimson.MeatPrisonArmor.KEY_MeatPrisonLegs;
+    
+    public override void SetStaticDefaults() {
+        Main.buffNoSave[Type] = true;
+        Main.buffNoTimeDisplay[Type] = true;
+    }
+
+    public override void Update(Player player, ref int buffIndex) {
+        var wardenActive = false;
+        for (int i = 0; i < Main.maxProjectiles; i++) {
+            var proj = Main.projectile[i];
+            if (proj.active && proj.owner == player.whoAmI && proj.type == ModContent.ProjectileType<BloodWarden>()) {
+                wardenActive = true;
+                break;
+            }
+        }
+
+        if (wardenActive) {
+            player.buffTime[buffIndex] = 18000;
+        }
+        else {
+            player.DelBuff(buffIndex);
+            buffIndex--;
+        }
+    }
 }

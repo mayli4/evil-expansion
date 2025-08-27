3eadfc6fa97ee7afc3347329b9907f3f20e40c19
scss28 <83139747+scss28@users.noreply.github.com>
2025-08-27 19:51:13 +0200

    wip: meat axe

---
+++ src/EvilExpansionMod/Content/Items/Crimson/_MeatAxe/MeatAxeHeldProjectile.cs
@@ -0,0 +1,128 @@
+﻿using EvilExpansionMod.Common.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Linq;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ModLoader;
+
+namespace EvilExpansionMod.Content.Items.Crimson;
+public class MeatAxeHeldProjectile : ModProjectile {
+    Player Owner => Main.player[Projectile.owner];
+    float Progress => 1f - (float)Owner.itemAnimation / Owner.itemAnimationMax;
+
+    Vector2[] _trailPositions;
+    Vector2 _rotationVector;
+
+    ref float TargetRotation => ref Projectile.ai[0];
+
+    public override string Texture => Assets.Assets.Textures.Items.Crimson.MeatAxe.KEY_MeatAxeItem;
+    public override void SetDefaults() {
+        Projectile.width = 0;
+        Projectile.height = 0;
+        Projectile.aiStyle = -1;
+        Projectile.penetrate = -1;
+        Projectile.ignoreWater = true;
+        Projectile.tileCollide = false;
+        Projectile.friendly = true;
+        Projectile.hostile = false;
+        Projectile.DamageType = DamageClass.Melee;
+        Projectile.timeLeft = 999;
+        Projectile.ownerHitCheck = true;
+        Projectile.localNPCHitCooldown = 999;
+        Projectile.usesLocalNPCImmunity = true;
+    }
+
+    public override bool ShouldUpdatePosition() => false;
+
+    public override void OnSpawn(IEntitySource source) {
+        TargetRotation = Owner.Center.DirectionTo(Main.MouseWorld).ToRotation();
+    }
+
+    public override bool PreAI() {
+        if(Owner.HeldItem.type != ModContent.ItemType<MeatAxeItem>() || Owner.ItemAnimationEndingOrEnded) {
+            Projectile.Kill();
+            return false;
+        }
+
+        Owner.heldProj = Projectile.whoAmI;
+        return true;
+    }
+
+    public override void AI() {
+        var arc = 1.5f * MathF.PI * Owner.direction;
+
+        var t = 3f * MathF.Pow(Progress, 2) - 2f * MathF.Pow(Progress, 3) - 0.3f * (1f - Progress) * MathF.Sin(MathHelper.Pi * Progress);
+        Projectile.rotation = TargetRotation - arc / 2f + arc * t;
+
+        _rotationVector = Projectile.rotation.ToRotationVector2();
+
+        Owner.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, Projectile.rotation - MathHelper.PiOver2);
+        Projectile.position = Owner.RotatedRelativePoint(Owner.MountedCenter) + new Vector2(-4 * Owner.direction, -2);
+
+        var trailLastPosition = _rotationVector;
+        _trailPositions ??= [.. Enumerable.Repeat(trailLastPosition, 5)];
+
+        for(var i = _trailPositions.Length - 1; i > 0; i--) {
+            _trailPositions[i] = _trailPositions[i - 1];
+        }
+        _trailPositions[0] = trailLastPosition;
+    }
+
+    public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox) {
+        float _ = 0;
+        return Collision.CheckAABBvLineCollision(
+            targetHitbox.TopLeft(),
+            targetHitbox.Size(),
+            Projectile.position,
+            Projectile.position + _rotationVector * 90f,
+            40,
+            ref _
+        );
+    }
+
+    public override bool PreDraw(ref Color lightColor) {
+        if(Progress > 0.15f) {
+            Graphics.BeginPipeline(0.5f)
+                .DrawBasicTrail(
+                    _trailPositions.Select(
+                        p => Projectile.position + p * 85f + p.RotatedBy(MathF.PI / 2f) * 10f
+                    ).ToArray(),
+                    static t => (1f - t) * 3f,
+                    TextureAssets.MagicPixel.Value,
+                    static t => Color.Lerp(Color.White, Color.Transparent, t)
+                )
+                .Flush();
+
+            Graphics.BeginPipeline(0.5f)
+                .DrawBasicTrail(
+                    _trailPositions.Select(
+                        p => Projectile.position + p * 65f - p.RotatedBy(MathF.PI / 2f) * 18f
+                    ).ToArray(),
+                    static t => (1f - t) * 5f,
+                    TextureAssets.MagicPixel.Value,
+                    static t => Color.Lerp(Color.White, Color.Transparent, t)
+                )
+                .Flush();
+        }
+
+        var texture = TextureAssets.Projectile[Type].Value;
+        var origin = new Vector2(0, 64);
+
+        Main.spriteBatch.Draw(
+            texture,
+            Projectile.position - Main.screenPosition,
+            null,
+            lightColor,
+            Projectile.rotation + (Owner.direction > 0 ? MathHelper.PiOver4 : MathHelper.PiOver4 * 3),
+            Owner.direction > 0 ? origin : new Vector2(texture.Width - origin.X, origin.Y),
+            Projectile.scale,
+            Owner.direction > 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally,
+            0f
+        );
+
+        return false;
+    }
+}
--- src/EvilExpansionMod/Content/Items/Crimson/_MeatAxe/MeatAxeItem.cs
+++ src/EvilExpansionMod/Content/Items/Crimson/_MeatAxe/MeatAxeItem.cs
@@ -3,10 +3,9 @@ using Terraria.GameContent.Creative;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace EvilExpansionMod.Content.Items.Corruption;
-
-public class LamethrowerItem : ModItem {
-    public override string Texture => Assets.Assets.Textures.Items.Corruption.Lamethrower.KEY_LamethrowerItem;
+namespace EvilExpansionMod.Content.Items.Crimson;
+public class MeatAxeItem : ModItem {
+    public override string Texture => Assets.Assets.Textures.Items.Crimson.MeatAxe.KEY_MeatAxeItem;
 
     public override void SetStaticDefaults() {
         CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 3;
@@ -15,24 +14,24 @@ public class LamethrowerItem : ModItem {
     public override void SetDefaults() {
         Item.damage = 25;
         Item.crit = 4;
-        Item.DamageType = DamageClass.Ranged;
+        Item.DamageType = DamageClass.Melee;
         Item.knockBack = 8;
 
-        Item.width = 68;
-        Item.height = 46;
+        Item.width = Item.height = 80;
 
-        Item.useTime = Item.useAnimation = 50;
-        Item.useStyle = ItemUseStyleID.Shoot;
+        Item.useTime = Item.useAnimation = 26;
+        Item.useStyle = -1;
 
         Item.value = 17500;
-        Item.rare = ItemRarityID.Yellow;
+        Item.rare = ItemRarityID.Green;
 
         Item.noUseGraphic = true;
         Item.noMelee = true;
 
-        Item.shoot = ModContent.ProjectileType<LamethrowerHeldProjectile>();
-        Item.shootSpeed = 6f;
+        Item.shoot = ModContent.ProjectileType<MeatAxeHeldProjectile>();
+        Item.shootSpeed = 9f;
 
+        Item.reuseDelay = 0;
         Item.channel = true;
         Item.useTurn = false;
     }

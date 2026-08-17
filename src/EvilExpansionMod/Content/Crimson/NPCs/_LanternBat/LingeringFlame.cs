7f62e2bb1d54ad2d0a07afc4746abf82222a5dc9
scss28 <83139747+scss28@users.noreply.github.com>
2026-08-17 19:06:21 +0200

    feat: bat lingering fire

--- src/EvilExpansionMod/Content/Crimson/NPCs/_LanternBat/LingeringFlame.cs
+++ src/EvilExpansionMod/Content/Crimson/NPCs/_LanternBat/LingeringFlame.cs
@@ -1,10 +1,9 @@
 using EvilExpansionMod.Common.Graphics;
-using EvilExpansionMod.Content.Dusts;
 using EvilExpansionMod.Utilities;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
-using System.Collections.Generic;
+using System.Linq;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -14,13 +13,17 @@ namespace EvilExpansionMod.Content.Crimson;
 public class LingeringFlameProjectile : ModProjectile {
     public override string Texture => "Terraria/Images/Projectile_0";
 
-    public int ParentNPCID => (int)Projectile.ai[0];
+    public NPC ParentNPC => Main.npc[(int)Projectile.ai[0]];
 
-    private int max_lifetime = 60 * 5;
+    static int MAX_LIFETIME = 160;
+
+    int _freePositionCount;
+    Vector2[] _trailPositions = null!;
+    readonly Vector2[] _trailVelocities = new Vector2[8];
 
     public override void SetDefaults() {
-        Projectile.width = 90;
-        Projectile.height = 90;
+        Projectile.width = 0;
+        Projectile.height = 0;
         Projectile.friendly = false;
         Projectile.hostile = true;
         Projectile.damage = 1;
@@ -28,7 +31,7 @@ public class LingeringFlameProjectile : ModProjectile {
         Projectile.tileCollide = false;
         Projectile.ignoreWater = true;
         Projectile.penetrate = -1;
-        Projectile.timeLeft = max_lifetime;
+        Projectile.timeLeft = MAX_LIFETIME;
         Projectile.aiStyle = -1;
         Projectile.alpha = 255;
         Projectile.usesLocalNPCImmunity = true;
@@ -38,142 +41,75 @@ public class LingeringFlameProjectile : ModProjectile {
     public override bool ShouldUpdatePosition() => false;
 
     public override void AI() {
-        if(Projectile.timeLeft > max_lifetime / 2 && Projectile.alpha > 0) {
-            Projectile.alpha -= 15;
-            if(Projectile.alpha < 0) Projectile.alpha = 0;
-        }
-        else if(Projectile.timeLeft < 30 && Projectile.alpha < 255) {
-            Projectile.alpha += 8;
-            if(Projectile.alpha > 255) Projectile.alpha = 255;
-        }
+        Projectile.direction = (int)Projectile.ai[1];
+        if(_freePositionCount < _trailVelocities.Length) {
+            Projectile.Center = ParentNPC.Center + Vector2.UnitY * 30f;
 
-        if(Main.rand.NextBool(15)) {
-            Dust.NewDust(
-                Projectile.position,
-                100,
-                100,
-                DustID.Firefly,
-                Main.rand.NextFloat(-1f, 1f),
-                Main.rand.NextFloat(-1f, 1f) - 0.5f,
-                100,
-                default,
-                Main.rand.NextFloat(0.8f, 1.2f)
-            );
+            _trailPositions ??= [.. Enumerable.Repeat(Projectile.Center, _trailVelocities.Length)];
+            if(_freePositionCount == 0 || Projectile.timeLeft % 8 == 0) {
+                _freePositionCount++;
+            }
+
+            for(var i = _freePositionCount; i < _trailPositions.Length; i++) {
+                _trailPositions[i] = Projectile.Center;
+                _trailVelocities[i] = Vector2.Zero;
+            }
         }
 
-        var newDustData = new Smoke.Data()
-        {
-            InitialLifetime = 40,
-            ElapsedFrames = 0,
-            InitialOpacity = 0.5f,
-            ColorStart = Color.Black,
-            ColorFade = new Color(69, 69, 113),
-            Spin = 0f,
-            InitialScale = Main.rand.NextFloat(0.5f, 2f)
-        };
+        for(var i = _freePositionCount - 1; i >= 0; i--) {
+            _trailPositions[i] += _trailVelocities[i];
+            _trailVelocities[i] += Vector2.UnitY * 0.075f * (MathF.Sin(Main.GameUpdateCount * 0.15f + i + Projectile.timeLeft * 0.05f) * 0.15f + 1f);
 
-        if(Main.rand.NextBool(15)) {
-            var newDust = Dust.NewDustPerfect(
-                Projectile.Center - new Vector2(0, 30),
-                ModContent.DustType<Smoke>(),
-                null,
-                0,
-                newColor: Color.White,
-                newDustData.InitialScale
-            );
-
-            newDust.customData = newDustData;
+            Lighting.AddLight(_trailPositions[i], TorchID.Orange);
         }
-
-        Lighting.AddLight(Projectile.Center, Color.OrangeRed.ToVector3());
     }
 
-    public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI) {
-        overWiresUI.Add(index);
+    public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox) {
+        if(Projectile.timeLeft < MAX_LIFETIME - 80) return false;
+
+        for(var i = 0; i < _trailPositions.Length - 1; i++) {
+            var lineStart = _trailPositions[i];
+            var lineEnd = _trailPositions[i + 1];
+
+            float _ = 0f;
+            if(Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), lineStart, lineEnd, 30f, ref _)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public override bool PreDraw(ref Color lightColor) {
-        var flameShader = Assets.Effects.Pixel.LingeringFlame.Asset.Value;
-        var noiseTexture1 = Assets.Textures.Sample.Pebbles.Asset.Value;
-        var circleTexture = Assets.Textures.Misc.Circle.Asset.Value;
-        var glowTexture = Assets.Textures.Sample.Glow1.Asset.Value;
+        var flameEffect = Assets.Effects.Trail.BatLingeringFlame.Asset.Value;
 
-
-        float flameScaleFactor = 1f;
-        if(Projectile.timeLeft < max_lifetime / 2) {
-            flameScaleFactor = Projectile.timeLeft / (max_lifetime / 2f);
-        }
-        flameScaleFactor = MathHelper.Clamp(flameScaleFactor, 0.1f, 1f);
-        float currentFlameSize = 1.2f * flameScaleFactor;
-
-        float alphaFactor = (1f - Projectile.alpha / 255f);
-        currentFlameSize *= alphaFactor;
-        currentFlameSize = Math.Max(currentFlameSize, 0.0f);
+        var colorA = Color.Yellow;
+        var colorB = Color.Red;
 
         Renderer.BeginPipeline(0.5f, Graphics.WorldTransformMatrix)
+            // uImage0
+            .SetTexture(0, Assets.Textures.Sample.Noise4.Asset.Value)
+            .SetSamplerState(0, SamplerState.PointWrap)
+            // uImage1
+            .SetTexture(1, Assets.Textures.Sample.Noise2.Asset.Value)
+            .SetSamplerState(1, SamplerState.PointWrap)
+            // uImage2
+            .SetTexture(2, Assets.Textures.Sample.BubblyNoise.Asset.Value)
+            .SetSamplerState(2, SamplerState.PointWrap)
             .SetEffectParams(
-                flameShader,
-                ("time", 0.01f * Main.GameUpdateCount + Projectile.whoAmI + 10),
-                ("size", new Vector2(150, 150)),
-                ("flameColor", Color.Black.ToVector4() * 0.5f),
-                ("coreColor", Color.Black.ToVector4() * 0.5f),
-                ("noiseScale", 0.5f),
-                ("flameSize", currentFlameSize),
-                ("tex1", noiseTexture1)
-            )
-            .SetBlendState(BlendState.Additive)
-            .DrawTexture(new()
-            {
-                Texture = circleTexture,
-                Position = Projectile.position - new Vector2(55, 90),
-                Size = new(200, 200),
-                Color = Projectile.GetAlpha(lightColor),
-                Rotation = Projectile.rotation,
-                Effect = flameShader
-            })
-            .ApplyOutline(Color.Black * 0.4f)
+                flameEffect,
+                ("uTime", Main.GameUpdateCount * 0.01f),
+                ("uProgress", 1f - (float)Projectile.timeLeft / MAX_LIFETIME),
+                ("uDirection", Projectile.ai[1]),
+                ("uColorA", colorA),
+                ("uColorB", colorB))
+            .DrawTrail(
+                _trailPositions,
+                t => 65,
+                static _ => Color.White,
+                flameEffect)
+            .ApplyOutline(colorB)
             .End();
-
-        Renderer.BeginPipeline(0.5f, Graphics.WorldTransformMatrix)
-            .SetEffectParams(
-                flameShader,
-                ("time", 0.025f * Main.GameUpdateCount + Projectile.whoAmI + 10),
-                ("size", new Vector2(Projectile.width, Projectile.height)),
-                ("flameColor", new Color(255, 106, 0).ToVector4()),
-                ("coreColor", new Color(234, 255, 0).ToVector4()),
-                ("outerCoreColor", new Color(255, 150, 0).ToVector4()),
-                ("noiseScale", 1.0f),
-                ("flameSize", currentFlameSize),
-                ("tex1", noiseTexture1)
-            )
-            .SetBlendState(BlendState.Additive)
-            .DrawTexture(new()
-            {
-                Texture = circleTexture,
-                Position = Projectile.position - new Vector2(30, 50),
-                Size = new(150, 150),
-                Color = Projectile.GetAlpha(lightColor),
-                Rotation = Projectile.rotation,
-                Effect = flameShader
-            })
-            .ApplyOutline(new Color(255, 150, 0))
-            .End();
-
-        var snapshot = Main.spriteBatch.CaptureEndBegin(new() { BlendState = BlendState.Additive });
-
-        Main.spriteBatch.Draw(
-            glowTexture,
-            Projectile.Center - Main.screenPosition - new Vector2(),
-            null,
-            new Color(255, 106, 0) * 0.25f * (1f - Projectile.alpha / 255f),
-            0f,
-            glowTexture.Size() / 2f,
-            currentFlameSize,
-            SpriteEffects.None,
-            0f
-        );
-
-        Main.spriteBatch.EndBegin(snapshot);
 
         return false;
     }

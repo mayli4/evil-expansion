fbaf4a659f9c189e04d0e55dd0d7cb0b5407fadf
Sydney Mayfield <sydsleeper@gmail.com>
2026-08-29 00:54:39 -0400

    feat: make pipeline exts extension members

--- src/EvilExpansionMod/Utilities/_Extensions/RenderPipelineExtensions.cs
+++ src/EvilExpansionMod/Utilities/_Extensions/RenderPipelineExtensions.cs
@@ -5,23 +5,26 @@ using Terraria;
 namespace EvilExpansionMod.Utilities;
 
 internal static class RenderPipelineExtensions {
-    public static RenderPipeline ApplyTint(this RenderPipeline @this, Color color) {
-        return @this.ApplyEffect(Assets.Shaders.Pixel.Tint.Asset.Value, ("uColor", color));
+    extension(RenderPipeline @this) {
+        public RenderPipeline ApplyTint(Color color) {
+            return @this.ApplyEffect(Assets.Shaders.Pixel.Tint.Asset.Value, ("uColor", color));
+        }
+        
+        public RenderPipeline ApplyOutline(Color color, float threshold = 0.001f) {
+            return @this.ApplyEffect(
+                Assets.Shaders.Pixel.Outline.Asset.Value,
+                ("uColor", color),
+                ("uThreshold", threshold),
+                ("uSize", Main.ScreenSize.ToVector2() * 2f));
+        }
+        
+        public RenderPipeline ApplyBloom(float intensity = 1.5f, float threshold = 0.5f) {
+            return @this.ApplyEffect(
+                Assets.Shaders.Pixel.Bloom.Asset.Value,
+                ("uThreshold", threshold),
+                ("uIntensity", intensity),
+                ("uSize", Main.ScreenSize.ToVector2() * 2f));
+        }
     }
 
-    public static RenderPipeline ApplyOutline(this RenderPipeline @this, Color color, float threshold = 0.001f) {
-        return @this.ApplyEffect(
-            Assets.Shaders.Pixel.Outline.Asset.Value,
-            ("uColor", color),
-            ("uThreshold", threshold),
-            ("uSize", Main.ScreenSize.ToVector2() * 2f));
-    }
-
-    public static RenderPipeline ApplyBloom(this RenderPipeline @this, float intensity = 1.5f, float threshold = 0.5f) {
-        return @this.ApplyEffect(
-            Assets.Shaders.Pixel.Bloom.Asset.Value,
-            ("uThreshold", threshold),
-            ("uIntensity", intensity),
-            ("uSize", Main.ScreenSize.ToVector2() * 2f));
-    }
 }

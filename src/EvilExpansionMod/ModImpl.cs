652a2ebb559dd34fb2653d4e160eedd9e5d73b4a
Roton <140523534+RotonDev@users.noreply.github.com>
2026-09-09 19:59:48 +0200

    fix: set icon_small manually

--- src/EvilExpansionMod/ModImpl.cs
+++ src/EvilExpansionMod/ModImpl.cs
@@ -1,8 +1,17 @@
 using Daybreak.Common.Features.Authorship;
+using Daybreak.Common.Features.Hooks;
 using Daybreak.Common.Features.ModPanel;
+using EvilExpansionMod.Core;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.ModLoader;
 
 namespace EvilExpansionMod;
 
 partial class ModImpl : IHasCustomAuthorMessage {
     public string GetAuthorText() => AuthorText.GetAuthorTooltip(this, "Made by:");
+    
+    [OnLoad]
+    private static void SetSmallIcon() {
+        ModContent.GetInstance<ModImpl>().SmallModIcon = ModContent.Request<Texture2D>(AssetReferences.icon_small.KEY);
+    }
 }

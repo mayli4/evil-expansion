f1ec2f43cd7ea5fa8a3b5cec0503bf979ef56561
Sydney Mayfield <sydsleeper@gmail.com>
2026-09-14 21:13:23 -0400

    feat: curseknight helm trail

--- src/EvilExpansionMod/Content/Corruption/Items/_CurseknightsHelm/CurseknightsHelmDrawLayer.cs
+++ src/EvilExpansionMod/Content/Corruption/Items/_CurseknightsHelm/CurseknightsHelmDrawLayer.cs
@@ -6,6 +6,7 @@ using Microsoft.Xna.Framework.Graphics;
 using System;
 using Terraria;
 using Terraria.DataStructures;
+using Terraria.GameContent;
 using Terraria.ModLoader;
 
 namespace EvilExpansionMod.Content.Corruption.Items._CurseknightsHelm;
@@ -33,7 +34,7 @@ internal class CurseknightsHelmDrawLayer : PlayerDrawLayer {
         var modPlayer = drawInfo.drawPlayer.GetModPlayer<CurseknightsHelmPlayer>();
         return modPlayer.IsWearingHelm && !modPlayer.HideVisual;
     }
-
+    
     protected override void Draw(ref PlayerDrawSet drawInfo) {
         var drawPlayer = drawInfo.drawPlayer;
 
@@ -44,48 +45,85 @@ internal class CurseknightsHelmDrawLayer : PlayerDrawLayer {
 
         var immuneAlphaMultiplier = ((255f - drawInfo.drawPlayer.immuneAlpha) / 255f);
 
-        var fireParticlePlayer = drawPlayer.GetModPlayer<CurseknightFireParticlePlayer>();
-
-        var fireTexture = Assets.Images.Misc.Circle.Asset.Value;
-        var glowTexture = Assets.Images.Sample.Glow1.Asset.Value;
-
-        var fireColor = new Color(0.901f, 1f, 0);
+        var circleTexture = Assets.Images.Misc.Circle.Asset.Value;
+        
+        const int flamePoints = 9;
+        const float flameSpacing = 7f;
+        const float flameWidth = 22f;
 
         using(var scope = _targetLease.Scope(true, Color.Transparent)) {
             using var pipeline = Graphics.Begin(0.5f * Main.GameViewMatrix.Zoom.X, Matrix.Identity);
             pipeline.SetBlendState(BlendState.Additive);
+            
+            var glowTexture = Assets.Images.Sample.Glow1.Asset.Value;
+            
+            if(CurseknightsHelm.HelmExploded) {
+                var anchor = headPosition - drawInfo.headVect + new Vector2(drawPlayer.bodyFrame.Width * 0.5f, 18f) + Main.screenPosition;
+                anchor += Main.OffsetsPlayerHeadgear[drawPlayer.bodyFrame.Y / drawPlayer.bodyFrame.Height] * drawPlayer.gravDir;
+                var wobble = 0.1f * Main.GameUpdateCount + drawPlayer.whoAmI * 0.7f;
+                
+                var flow = new Vector2(-drawPlayer.direction * 0.6f, -1f).SafeNormalize(Vector2.UnitY);
+                var sideways = new Vector2(-flow.Y, flow.X);
 
-            foreach(var particle in fireParticlePlayer.Particles) {
-                var progress = (float)particle.TimeLeft / CurseknightFireParticle.MaxTimeLeft;
-                var color = Color.Lerp(fireColor, new Color(0.2f, 0.2f, 0.2f, 0f), MathF.Max(0f, 1f - particle.Alpha * progress * immuneAlphaMultiplier));
+                Span<Vector2> flame = new Vector2[flamePoints];
+                for(var i = 0; i < flamePoints; i++) {
+                    var t = i / (float)(flamePoints - 1);
+                    flame[i] = anchor
+                               + flow * (i * flameSpacing)
+                               + sideways * MathF.Sin(wobble - i * 0.7f) * (1.5f + 4.5f * t)
+                               - drawPlayer.velocity * (i * 0.45f)
+                               - Main.screenPosition;
+                }
+                
+                const float anchorBoundsRadius = 50f;
 
-                pipeline.DrawTexture(new()
-                {
-                    Texture = fireTexture,
-                    Position = particle.Position - Main.screenPosition,
-                    Scale = new Vector2(1f, 0.65f) * (0.2f + progress * 0.25f) * particle.Scale,
-                    Origin = fireTexture.Size() / 2f,
-                    Rotation = particle.Rotation,
-                    Color = color,
-                });
+                var fire = Assets.Shaders.Pixel.CursedSpiritFire.Asset.Value;
+                pipeline
+                    .SetTexture(0, Assets.Images.Sample.Noise2.Asset.Value)
+                    .SetTexture(1, Assets.Images.Sample.Noise6.Asset.Value)
+                    .SetEffectParams(
+                        fire,
+                        ("uTime", 0.025f * Main.GameUpdateCount + drawPlayer.whoAmI * 3.432f),
+                        ("uStepY", 0.095f),
+                        ("uColor1", CursedSpiritNPC.GhostColor2),
+                        ("uColor2", new Color(230, 255, 0)),
+                        ("uStepColor", 0.05f),
+                        ("uScale", 0.65f))
+                    .DrawTrail(
+                        flame,
+                        static t => flameWidth * (1f - t) + 10f,
+                        _ => Color.White * immuneAlphaMultiplier,
+                        fire)
+                    // need this so the circle doesnt jitter? assuming due to aggressive viewport crop matrix
+                    .DrawTexture(new DrawTextureOptions
+                    {
+                        Texture = TextureAssets.MagicPixel.Value,
+                        Position = anchor - new Vector2(anchorBoundsRadius) - Main.screenPosition,
+                        Size = new Vector2(anchorBoundsRadius * 2f),
+                        Color = Color.Transparent,
+                    })
+                    .DrawTexture(new DrawTextureOptions
+                    {
+                        Texture = circleTexture,
+                        Position = anchor - Main.screenPosition,
+                        Color = CursedSpiritNPC.GhostColor2 * immuneAlphaMultiplier,
+                        Origin = 16f * Vector2.One,
+                        Scale = Vector2.One * 0.70f,
+                    })
+                    .ApplyOutline(CursedSpiritNPC.GhostColor1 * 0.5f)
+                    .DrawTexture(new DrawTextureOptions
+                    {
+                        Texture = glowTexture,
+                        Position = anchor - Main.screenPosition,
+                        Color = CursedSpiritNPC.GhostColor1 * 0.45f * immuneAlphaMultiplier,
+                        Origin = glowTexture.Size() / 2f,
+                        Scale = Vector2.One * 0.15f,
+                    })
+                    .ApplyBloom(0.2f);
             }
-
-            // foreach(var particle in fireParticlePlayer.Particles) {
-            //     var progress = (float)particle.TimeLeft / CurseknightFireParticle.MaxTimeLeft;
-            //     pipeline.DrawTexture(new()
-            //     {
-            //         Texture = glowTexture,
-            //         Position = particle.Position - Main.screenPosition,
-            //         Scale = Vector2.One * progress * 0.25f,
-            //         Origin = glowTexture.Size() / 2f,
-            //         Color = fireColor * 0.1f * immuneAlphaMultiplier,
-            //     });
-            // }
-
-            pipeline.ApplyBloom(0.5f);
         }
 
-        drawInfo.DrawDataCache.Add(new(
+        drawInfo.DrawDataCache.Add(new DrawData(
             _targetLease.Target,
             Vector2.Zero,
             Color.White));
